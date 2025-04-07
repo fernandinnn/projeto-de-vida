04c2bf8979f79d11c65cc20713283323fd4fe818
viadinho!!! <146236941+fernandinnn@users.noreply.github.com>
2025-04-07 08:29:18 -0300

    .

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
+
+for (let i=0; i <botoes.length; i++){
+    botoes [i].onclick = function(){
+        
+    }
+}
 console.log(botoes);
 for (let i = 0; i < botoes.length; i++) {
     console.log(i);
